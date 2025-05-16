af018f5b938cf4bfbbb080db2a3a826b340e873a
mmanjhu555 <monuytfree1@gmail.com>
2025-05-16 21:57:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Cobra'
-
+    return 'Hello from Monu Choudhary'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
-
+    app.run(host='0.0.0.0', port=5000)
